8965e2b522aaed730035de323a3fba3c404b8207
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 19:24:29 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -7,6 +7,11 @@ const HomePage = () => {
         <div className="" style={{ paddingTop: '40px'}}>
             <center>
                 <img src="https://avatars.githubusercontent.com/u/139304882?s=200&v=4" alt="" style={{ borderRadius: '5px'}}/>
+                <div className="" style={{ paddingTop: '30px'}}>
+                    <p className="font-semibold" style={{ fontSize: '40px' }}>The JKCSS</p>
+                    <p className="">CSS Framework</p>
+                    <p className="">The First CSS Framework Developed in Sri Lanka</p>
+                </div>
             </center>         
         </div>
     </div>
