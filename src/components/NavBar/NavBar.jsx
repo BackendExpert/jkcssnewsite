68d60417fc2d39f90020ef8c7deb50612384e83d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 18:08:37 +0530

    Update NavBar.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import '../../styles/index.css'
 import '@jehankandy/jkcss/src/jkcss.css'
 import JKCSS from '../../assets/jkcss.png'
 import { Link } from 'react-router-dom'
-
+import { BsLinkedin } from "react-icons/bs";
 
 const NavBar = () => {
   return (
@@ -14,6 +14,9 @@ const NavBar = () => {
                     <img src={JKCSS} alt="" className='logo'/>
                 </Link>
                 <h1 className="font-semibold">JKCSS</h1>
+            </div>
+            <div className="">
+                <BsLinkedin style={{ height: '20px', width: 'auto'}} />
             </div>
         
         </div>
