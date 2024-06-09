8afd4ff60beacac1c09f9e1e6fc23e65f29e028c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 18:05:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import '../../styles/index.css'
 import '@jehankandy/jkcss/src/jkcss.css'
 import JKCSS from '../../assets/jkcss.png'
+import { Link } from 'react-router-dom'
+
 
 const NavBar = () => {
   return (
     <div className='container navbar'>
         <div className="flex justify-between">
             <div className="flex">
-                <img src={JKCSS} alt="" className='logo'/>
+                <Link to={'/'}>
+                    <img src={JKCSS} alt="" className='logo'/>
+                </Link>
                 <h1 className="font-semibold">JKCSS</h1>
             </div>
         
