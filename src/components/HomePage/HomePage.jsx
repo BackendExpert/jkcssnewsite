278a22becd66383b8eb37fe44dbed36354413654
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 19:17:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,8 +4,10 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className="site-container">
-        <div className="img-list">
-          <img src="https://avatars.githubusercontent.com/u/139304882?s=200&v=4" alt="" style={{ borderRadius: '5px'}}/>
+        <div className="" style={{ paddingTop: '40px'}}>
+            <center>
+                <img src="https://avatars.githubusercontent.com/u/139304882?s=200&v=4" alt="" style={{ borderRadius: '5px'}}/>
+            </center>         
         </div>
     </div>
   )
