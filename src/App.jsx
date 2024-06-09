aa7fc4a7e71182bd2064c0b6925881bc8ecf128b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 19:04:25 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from './components/HomePage/HomePage';
-import NavBar from '../NavBar/NavBar'
+import NavBar from './components/NavBar/NavBar';
+
 
 export default function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
-        <NavBar />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
