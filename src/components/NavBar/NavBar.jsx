1a1a7fa2ff78bd6fbbaf50c823e877d3b405b9ff
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 18:18:34 +0530

    Update NavBar.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,9 +15,16 @@ const NavBar = () => {
                 </Link>
                 <h1 className="font-semibold">JKCSS</h1>
             </div>
+            <div className="">
+                <ul>
+                    <li>HI akk</li>
+                    <li>HI akk</li>
+                    <li>HI akk</li>
+                </ul>              
+            </div>
             <div className="" style={{ paddingTop: '6px' }}>
-                <Link to={'https://www.linkedin.com/in/jehan-weerasuriya-23a3a4223/'}><BsLinkedin style={{ height: '30px', width: 'auto', paddingRight: '10px'}} /></Link>
-                <Link to={'https://github.com/JKCSS-CSS-Framework'}><BsGithub style={{ height: '30px', width: 'auto', paddingRight: '10px'}} /></Link>
+                <Link to={'https://www.linkedin.com/in/jehan-weerasuriya-23a3a4223/'}><BsLinkedin style={{ height: '30px', width: 'auto', paddingRight: '10px', color: 'white'}} /></Link>
+                <Link to={'https://github.com/JKCSS-CSS-Framework'}><BsGithub style={{ height: '30px', width: 'auto', paddingRight: '10px', color: 'white'}} /></Link>
             </div>
         
         </div>
